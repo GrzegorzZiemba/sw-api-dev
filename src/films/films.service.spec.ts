410eb569cebf56cfdbef97619720ddc0f4f5ac37
GrzegorzZiemba <gzgrzegorzziemba@gmail.com>
2023-12-08 17:56:57 +0100

    Added some test to services from species and films

--- src/films/films.service.spec.ts
+++ src/films/films.service.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmsService } from './films.service';
+import { FilmCache } from './film-cache.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import axios from 'axios';
+import { NotFoundException, HttpException } from '@nestjs/common';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('FilmsService', () => {
   let service: FilmsService;
+  let mockFilmCacheRepository;
 
   beforeEach(async () => {
+    mockFilmCacheRepository = {
+      findOne: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilmsService],
+      providers: [
+        FilmsService,
+        {
+          provide: getRepositoryToken(FilmCache),
+          useValue: mockFilmCacheRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<FilmsService>(FilmsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should save a film to cache if not found in cache and found in API', async () => {
+    mockFilmCacheRepository.findOne.mockResolvedValue(null);
+    const filmData = { episode_id: '1', title: 'Test Film', opening_crawl: 'Test Opening Crawl' };
+    mockedAxios.get.mockResolvedValue({ data: filmData });
+  
+    await service.findOneById(1);
+  
+    expect(mockFilmCacheRepository.save).toHaveBeenCalledWith({
+      filmId: 'film-1',
+      data: expect.anything(), // Możesz tutaj użyć bardziej szczegółowego sprawdzenia
+      lastFetched: expect.any(Date),
+    });
   });
+
+it('should return a film if it is found in cache', async () => {
+  const cachedFilm = { id: '1', title: 'Test Film', openingCrawl: 'Test Opening Crawl' };
+  mockFilmCacheRepository.findOne.mockResolvedValue({ data: cachedFilm });
+
+  const result = await service.findOneById(1);
+  expect(result).toEqual(cachedFilm);
+});
+
+  
+
+  it('should throw NotFoundException if film is not found', async () => {
+    mockFilmCacheRepository.findOne.mockResolvedValue(null);
+    mockedAxios.get.mockRejectedValue({ response: { status: 404 } });
+  
+    await expect(service.findOneById(1)).rejects.toThrow(NotFoundException);
+  });
+  
+
 });
--- src/species/species.service.spec.ts
+++ src/species/species.service.spec.ts
@@ -1,18 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SpeciesService } from './species.service';
+import { SpeciesCache } from './species-cache.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import axios from 'axios';
+import { NotFoundException, HttpException } from '@nestjs/common';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('SpeciesService', () => {
   let service: SpeciesService;
+  let mockSpeciesCacheRepository;
 
   beforeEach(async () => {
+    mockSpeciesCacheRepository = {
+      findOne: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SpeciesService],
+      providers: [
+        SpeciesService,
+        {
+          provide: getRepositoryToken(SpeciesCache),
+          useValue: mockSpeciesCacheRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<SpeciesService>(SpeciesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should fetch and cache species if not in cache', async () => {
+    mockSpeciesCacheRepository.findOne.mockResolvedValue(null);
+    const speciesData = { results: [{ name: 'Test Species', url: 'https://swapi.dev/api/species/1/' }] };
+    mockedAxios.get.mockResolvedValue({ data: speciesData });
+
+    const result = await service.findAll(1);
+
+    expect(mockSpeciesCacheRepository.save).toHaveBeenCalledWith(expect.anything());
+    expect(result).toEqual([{ id: '1', name: 'Test Species', url: 'https://swapi.dev/api/species/1/' }]);
+  });
+
+
+
+  it('should fetch and cache a specific species by ID if not in cache', async () => {
+    mockSpeciesCacheRepository.findOne.mockResolvedValue(null);
+    const speciesData = { name: 'Test Species', url: 'https://swapi.dev/api/species/1/' };
+    mockedAxios.get.mockResolvedValue({ data: speciesData });
+
+    const result = await service.findOneById(1);
+
+    expect(mockSpeciesCacheRepository.save).toHaveBeenCalledWith(expect.anything());
+    expect(result).toEqual({ id: '1', name: 'Test Species', url: 'https://swapi.dev/api/species/1/' });
+  });
+
+  it('should throw NotFoundException if species is not found by ID', async () => {
+    mockSpeciesCacheRepository.findOne.mockResolvedValue(null);
+    mockedAxios.get.mockRejectedValue({ response: { status: 404 } });
+
+    await expect(service.findOneById(999)).rejects.toThrow(NotFoundException);
   });
 });
